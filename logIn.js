40a63aab8bfeeb77b6d765dd4d1645d49f89dc94
Andrew Killian <ack97@txstate.edu>
2022-11-10 16:23:39 -0600

    added script

---
+++ logIn.js
@@ -0,0 +1,12 @@
+let head = document.getElementById("header");
+console.log(head);
+
+let input = document.getElementById('username')
+console.log(input);
+
+let fbButton = document.querySelector('.fb-login-button')
+
+fbButton.addEventListener('click', () => {
+    alert('poo');
+    console.log('poo');
+})
